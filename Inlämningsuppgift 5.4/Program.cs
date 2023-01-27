4b8db677b1af971ab03493ef66a89718af8e90b6
Elev <Elev@TEB125102>
2023-01-27 13:58:56 +0100

    Add project files.

---
+++ Inlämningsuppgift 5.4/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+namespace Uppgift_5_4
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            string b = (" ");
+            string[] a = new string[5];
+            a[0] = ("Håkan ");
+            a[1] = ("Adam ");
+            a[2] = ("David ");
+            a[3] = ("patrik ");
+            a[4] = ("Amin");
+            int j = 1;
+            int y = 1;
+
+            for (int i = 0; i < 5; i++)
+            {
+               
+                {
+                    y++;
+                }
+                while (y == 1)
+                {
+                    if (a[i][0] < a[j][0])
+                    {
+                        b = a[i];
+                    }
+                    y--;
+                }
+                if (b == " ")
+                {
+                    b = a[i];
+                }
+               if (a[i][0] < b[0])
+                {
+                    b = a[i];
+                }
+               if (b[0] == a[i][0] & a[i][1] != b[1])
+                {
+                    b = b + a[i];
+                }
+               
+            }
+            Console.WriteLine(b );
+        }
+    }
+}
